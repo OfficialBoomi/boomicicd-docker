984069e52946b604b7c9cf028e6abab0e6c828da
EC2 Default User <ec2-user@ip-172-31-1-17.us-east-2.compute.internal>
2019-11-25 02:49:41 +0000

    Updated entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,5 @@
 #! /bin/bash -e
-/sbin/tini -- /usr/local/bin/jenkins.sh
-java -jar ${JENKINS_CLI} -s ${JENKINS_URL} -auth ${JENKINS_USER}:${JENKINS_PASS} safe-restart
 sed -e "s/GIT_CONFIG_NAME/${GIT_CONFIG_NAME}/" -e "s/GIT_CONFIG_EMAIL/${GIT_CONFIG_EMAIL}/"  ${JENKINS_HOME}/hudson.plugins.git.GitSCM.xml_1 > ${JENKINS_HOME}/hudson.plugins.git.GitSCM.xml
 git config --global user.email ${GIT_CONFIG_EMAIL}
 git config --global user.name  ${GIT_CONFIG_USER}
+/sbin/tini -- /usr/local/bin/jenkins.sh
