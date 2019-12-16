aed7a18f9b762791eb15df1efa76796b00418793
root <root@W10G7QWNN2.mht.amer.dell.com>
2019-12-15 22:13:38 -0500

    New key file for container start

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,4 +2,11 @@
 sed -e "s/GIT_CONFIG_NAME/${GIT_CONFIG_NAME}/" -e "s/GIT_CONFIG_EMAIL/${GIT_CONFIG_EMAIL}/"  ${JENKINS_HOME}/hudson.plugins.git.GitSCM.xml_1 > ${JENKINS_HOME}/hudson.plugins.git.GitSCM.xml
 git config --global user.email ${GIT_CONFIG_EMAIL}
 git config --global user.name  ${GIT_CONFIG_USER}
-/sbin/tini -- /usr/local/bin/jenkins.sh
+
+SECRET=`cat ${REF}/secret`
+if [[ "${SECRET}" != "${KEY}" ]]
+then
+	exit 255
+else
+	/sbin/tini -- /usr/local/bin/jenkins.sh
+fi
