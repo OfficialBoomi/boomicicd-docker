6ac6a8af028bc8f6dc7376d2d5f18f69c8c56bf8
Ajay Natarajan <ajay.natarajan@dell.com>
2020-03-15 19:57:11 -0400

    Removed cli and jenkins_jobs folder into its own repository

--- docker/build.sh
+++ docker/build.sh
@@ -5,9 +5,15 @@ then
   echo "No tag for build"
   return 255
 fi
-dir=$(pwd)
-cd ${dir}/jenkins_jobs/jobs
+curdir=`pwd`
+echo ${curdir}
+cp -R ../../boomicicd-jenkinsjobs/jenkins_jobs jenkins_jobs
+cp -R ../../boomicicd-cli/cli cli
+cd ${curdir}/jenkins_jobs/jobs
 find . -type d -name workspace -exec rm -rf "{}" ";" 2>&1 > /dev/null
-cd ${dir}
+cd ${curdir}
+echo `pwd`
+cp -R ../../boomicicd-cli/cli cli
 docker build -t integrationguy/boomicicd:${tag} .
-
+rm -rf cli
+rm -rf jenkins_jobs
