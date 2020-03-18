d7ab50cae2fa8ad23fb1708342d3937566dace4a
Ajay Natarajan <ajay.natarajan@dell.com>
2020-03-18 11:29:51 -0400

    Updated build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-tag=$1
-if [[ null == "${tag}" ]]
+name=$1
+tag=$2
+
+if [[ null == "${name}" ]]
 then
-  echo "No tag for build"
+  echo "No name for the image"
   return 255
 fi
+
+if [[ null == "${tag}" ]]
+then
+  echo "No tag for the image"
+  return 255
+fi
+
 curdir=`pwd`
 echo ${curdir}
 cp -R ../../boomicicd-jenkinsjobs/jenkins_jobs jenkins_jobs
@@ -14,6 +23,6 @@ find . -type d -name workspace -exec rm -rf "{}" ";" 2>&1 > /dev/null
 cd ${curdir}
 echo `pwd`
 cp -R ../../boomicicd-cli/cli cli
-docker build -t integrationguy/boomicicd:${tag} .
+docker build -t ${name}:${tag} .
 rm -rf cli
 rm -rf jenkins_jobs
