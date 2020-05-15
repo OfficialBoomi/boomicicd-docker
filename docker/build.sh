8128a07e32b01997951e67eb73d1e74c43b67b4e
Boomi Admin <boomiadmin@flexport>
2020-05-15 15:19:50 -0400

    Latest docker image

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# The docker image copies the files from jenkins, cli and sonarqube repos to create the image
 name=$1
 tag=$2
 
@@ -15,14 +16,17 @@ then
 fi
 
 curdir=`pwd`
-echo ${curdir}
 cp -R ../../boomicicd-jenkinsjobs/jenkins_jobs jenkins_jobs
 cp -R ../../boomicicd-cli/cli cli
 cd ${curdir}/jenkins_jobs/jobs
 find . -type d -name workspace -exec rm -rf "{}" ";" 2>&1 > /dev/null
 cd ${curdir}
-echo `pwd`
+mkdir -p sonarqube
+unzip -q ../../boomicicd-cli/sonarqube/sonar*.zip -d sonarqube
+chmod u+x sonarqube/sonar*/bin/*
 cp -R ../../boomicicd-cli/cli cli
+rm -f cli/scripts/*.json
 docker build -t ${name}:${tag} .
 rm -rf cli
 rm -rf jenkins_jobs
+rm -rf sonarqube
