a01adb0f4fadcc66d5aae2c24b6f318c07782505
Boomi Admin <boomiadmin@flexport>
2020-05-26 15:26:30 -0400

    Removed regs to GitSCM.xml; updated Jenkins version

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -e
-sed -e "s/GIT_CONFIG_NAME/${GIT_CONFIG_NAME}/" -e "s/GIT_CONFIG_EMAIL/${GIT_CONFIG_EMAIL}/"  ${JENKINS_HOME}/hudson.plugins.git.GitSCM.xml_1 > ${JENKINS_HOME}/hudson.plugins.git.GitSCM.xml
-git config --global user.email ${GIT_CONFIG_EMAIL}
-git config --global user.name  ${GIT_CONFIG_USER}
+# sed -e "s/GIT_CONFIG_NAME/${GIT_CONFIG_NAME}/" -e "s/GIT_CONFIG_EMAIL/${GIT_CONFIG_EMAIL}/"  ${JENKINS_HOME}/hudson.plugins.git.GitSCM.xml_1 > ${JENKINS_HOME}/hudson.plugins.git.GitSCM.xml
+# git config --global user.email ${GIT_CONFIG_EMAIL}
+# git config --global user.name  ${GIT_CONFIG_USER}
 
 SECRET=`cat ${REF}/secret`
 if [[ "${SECRET}" != "${KEY}" ]]
